8b334cfb3f1177326325db52a80018838c8681c1
Anton Maminov <anton.linux@gmail.com>
2012-10-05 16:22:34 +0300

    vesion 0.0.7

--- lib/crowdin-api/methods.rb
+++ lib/crowdin-api/methods.rb
@@ -58,7 +58,7 @@ module Crowdin
     #
     # POST http://api.crowdin.net/api/project/{project-identifier}/update-file?key={project-key}
     #
-    def update_file(files)
+    def update_file(files, params = {})
       files = Hash[files.map{ |f| [
         f[:dest]               || raise(ArgumentError, "'`:dest` is required'"),
         ::File.open(f[:source] || raise(ArgumentError, "'`:source` is required'") )
--- lib/crowdin-api/version.rb
+++ lib/crowdin-api/version.rb
@@ -1,5 +1,5 @@
 module Crowdin
   class API
-    VERSION = "0.0.6"
+    VERSION = "0.0.7"
   end
 end
