0cb09bc69169c43d850a2ff8dab9d558f8300ab0
Anton Maminov <a.maminov@svitla.com>
2014-08-09 15:05:34 +0300

    move crowdin.net -> crowdin.com

--- lib/crowdin-api.rb
+++ lib/crowdin-api.rb
@@ -6,7 +6,7 @@ require "crowdin-api/errors"
 require "crowdin-api/methods"
 require "crowdin-api/version"
 
-# The Crowdin::API library is used for interactions with a crowdin.net website.
+# The Crowdin::API library is used for interactions with a crowdin.com website.
 #
 # == Example
 #
@@ -38,7 +38,7 @@ module Crowdin
       @api_key     = options.delete(:api_key)
       @project_id  = options.delete(:project_id)
       @account_key = options.delete(:account_key)
-      @base_url    = options.delete(:base_url) || 'http://api.crowdin.net'
+      @base_url    = options.delete(:base_url) || 'http://api.crowdin.com'
 
       @log = nil
 
