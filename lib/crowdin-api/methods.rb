ebc4c87a1f396c6e88ea34a83a36484825e2069c
Aleksandr Varnin <10187586+cmrd-senya@users.noreply.github.com>
2021-08-25 20:21:55 +0300

    Add first methods of APIv2

--- lib/crowdin-api/methods.rb
+++ lib/crowdin-api/methods.rb
@@ -7,7 +7,28 @@ module Crowdin
 
   class API
 
+    def list_directories(params = {})
+      request(
+        method: :get,
+        path: "/api/v2/projects/#{@project_id}/directories",
+        query: params
+      )
+    end
 
+    def list_branches(params = {})
+      request(
+        method: :get,
+        path: "/api/v2/projects/#{@project_id}/branches",
+        query: params
+      )
+    end
+
+    def delete_branch(branch_id)
+      request(
+        method: :delete,
+        path: "/api/v2/projects/#{@project_id}/branches/#{branch_id}"
+      )
+    end
 
   end
 end
