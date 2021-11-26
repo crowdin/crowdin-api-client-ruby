b41f092a348169d171ae3528335383ced29bea26
Korbut Kirill <kirill.korbut@roobykon.com>
2021-11-26 11:47:32 +0200

    Enterprise groups

--- lib/crowdin-api/api-resources/projects.rb
+++ lib/crowdin-api/api-resources/projects.rb
@@ -66,6 +66,72 @@ module Crowdin
         request.process_response!
       end
 
+      # For Enterprise API only
+
+      def list_groups(query={})
+        request = Web::Request.new(
+          @connection,
+          :get,
+          "#{@target_api_url}/groups",
+          query
+        )
+
+        request.process_request!
+        request.process_response!
+      end
+
+      def add_group(query={})
+        request = Web::Request.new(
+          @connection,
+          :post,
+          "#{@target_api_url}/groups",
+          query
+        )
+
+        request.process_request!
+        request.process_response!
+      end
+
+      def get_group(group_id=nil)
+        group_id || raise(ArgumentError, ':group_id is required')
+
+        request = Web::Request.new(
+          @connection,
+          :get,
+          "#{@target_api_url}/groups/#{group_id}"
+        )
+
+        request.process_request!
+        request.process_response!
+      end
+
+      def delete_group(group_id=nil)
+        group_id || raise(ArgumentError, ':group_id is required')
+
+        request = Web::Request.new(
+          @connection,
+          :delete,
+          "#{@target_api_url}/groups/#{group_id}"
+        )
+
+        request.process_request!
+        request.process_response!
+      end
+
+      def edit_group(group_id=nil, query={})
+        group_id || raise(ArgumentError, ':group_id is required')
+
+        request = Web::Request.new(
+          @connection,
+          :patch,
+          "#{@target_api_url}/groups/#{group_id}",
+          query
+        )
+
+        request.process_request!
+        request.process_response!
+      end
+
     end
   end
 end
