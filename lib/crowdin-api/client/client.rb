45407992a5bc662c6429f1fe4d380283765f871f
Korbut Kirill <kirill.korbut@roobykon.com>
2021-11-26 12:09:58 +0200

    disable logger by default

--- lib/crowdin-api/client/client.rb
+++ lib/crowdin-api/client/client.rb
@@ -14,7 +14,6 @@ module Crowdin
     # handle and parse validation erorrs after request
     # cover comments to all methods
 
-
     # For more information about method parameters see official crowdin api docs.
     include API::Languages
     include API::Projects
@@ -30,12 +29,14 @@ module Crowdin
     # == Settings
     #
     # * *api_key* [String] - the authentication API key can be found on the account settings page
-    # * *project_id* [String] - the project identifier, default - nil
+    # * *project_id* [Integer] - the project identifier, default - nil
+    # * *logger* [Boolean] - flag to require and enable logger
     # * *organization_domain* [String] - the name of your organization, only for enterprise
     #
     def initialize(options = {})
       @api_key    = options.delete(:api_key)
       @project_id = options.delete(:project_id) || nil
+      @logger     = options.delete(:logger)     || false
       @base_url   = options[:organization_domain] ?
                       "https://#{options.delete(:organization_domain)}.api.crowdin.com" : 'https://api.crowdin.com'
 
@@ -61,10 +62,14 @@ module Crowdin
     end
 
     def log!(message)
+      return true unless @logger
+
       log.debug(message)
     end
 
     def log=(logger)
+      raise 'You need to enable logger while Crowdin client initialization to change log instance' unless @logger
+
       @log = logger
     end
 
