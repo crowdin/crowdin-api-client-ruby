d09a1f484bbfca080254eafd774626e02327599e
Korbut Kirill <kirill.korbut@roobykon.com>
2021-12-09 17:02:46 +0200

    Tests revisions

--- spec/core/config-instance_spec.rb
+++ spec/core/config-instance_spec.rb
@@ -16,55 +16,57 @@ describe 'Config instance' do
     expect(@crowdin.config.api_token).to_not be_nil
   end
 
-  it 'should have a #base_url' do
-    expect(@crowdin.config.base_url).to_not be_nil
-  end
-
   it '#target_api_url should equal /api/v2 by default' do
     expect(@crowdin.config.target_api_url).to eq('/api/v2')
   end
 
-  it '#logger_enabled? should be false by default' do
-    expect(@crowdin.config.logger_enabled?).to be_falsey
-  end
-
-  it '#logger_enabled? should equal specified argument' do
-    @crowdin = Crowdin::Client.new do |config|
-      config.enable_logger = true
+  describe '#logger_enabled?' do
+    it 'should be false by default' do
+      expect(@crowdin.config.logger_enabled?).to be_falsey
     end
 
-    expect(@crowdin.config.logger_enabled?).to be_truthy
+    it 'should equal specified argument' do
+      @crowdin = Crowdin::Client.new do |config|
+        config.enable_logger = true
+      end
+
+      expect(@crowdin.config.logger_enabled?).to be_truthy
+    end
   end
 
-  it '#enterprise_mode? should be false by default' do
-    expect(@crowdin.config.enterprise_mode?).to be_falsey
-  end
-
-  it '#enterprise_mode? should equal specified arguments' do
-    @crowdin = Crowdin::Client.new do |config|
-      config.organization_domain = 'organization_domain'
+  describe '#enterprise_mode?' do
+    it 'should be false by default' do
+      expect(@crowdin.config.enterprise_mode?).to be_falsey
     end
 
-    expect(@crowdin.config.enterprise_mode?).to be_truthy
+    it 'should equal specified arguments' do
+      @crowdin = Crowdin::Client.new do |config|
+        config.organization_domain = 'organization_domain'
+      end
+
+      expect(@crowdin.config.enterprise_mode?).to be_truthy
+    end
   end
 
-  it '#base_url should equal https://api.crowdin.com by default' do
-    expect(@crowdin.config.base_url).to eql('https://api.crowdin.com')
-  end
-
-  it '#base_url should equal specified organization domain' do
-    @crowdin = Crowdin::Client.new do |config|
-      config.organization_domain = 'organization_domain'
+  describe '#base_url' do
+    it 'should equal https://api.crowdin.com by default' do
+      expect(@crowdin.config.base_url).to eq('https://api.crowdin.com')
     end
 
-    expect(@crowdin.config.base_url).to eql('https://organization_domain.api.crowdin.com')
-  end
+    it 'should equal specified organization domain' do
+      @crowdin = Crowdin::Client.new do |config|
+        config.organization_domain = 'organization_domain'
+      end
 
-  it '#base_url should equal full specified organization domain when user specify full url (with .com)' do
-    @crowdin = Crowdin::Client.new do |config|
-      config.organization_domain = 'organization_domain.com'
+      expect(@crowdin.config.base_url).to eq('https://organization_domain.api.crowdin.com')
     end
 
-    expect(@crowdin.config.base_url).to eql('organization_domain.com')
+    it 'should equal full specified organization domain when user specify full url (with .com)' do
+      @crowdin = Crowdin::Client.new do |config|
+        config.organization_domain = 'organization_domain.com'
+      end
+
+      expect(@crowdin.config.base_url).to eq('organization_domain.com')
+    end
   end
 end
