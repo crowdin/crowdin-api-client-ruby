770f04d7b6ac26f22feb1d22c1db0a5cf8a594dc
Korbut Kirill <kirill.korbut@roobykon.com>
2021-12-13 22:47:49 +0200

    Translation Status project_id revision

--- lib/crowdin-api/api-resources/translation_status.rb
+++ lib/crowdin-api/api-resources/translation_status.rb
@@ -3,11 +3,9 @@
 module Crowdin
   module ApiResources
     module TranslationStatus
-      def get_branch_progress(branch_id = nil, query = {})
-        project_id = query[:project_id] || config.project_id
-
-        branch_id   || raise_parameter_is_required_error(:branch_id)
-        project_id  || raise_project_id_is_required_error
+      def get_branch_progress(branch_id = nil, query = {}, project_id = config.project_id)
+        branch_id  || raise_parameter_is_required_error(:branch_id)
+        project_id || raise_project_id_is_required_error
 
         request = Web::Request.new(
           self,
@@ -19,9 +17,7 @@ module Crowdin
         request.perform
       end
 
-      def get_directory_progress(directory_id = nil, query = {})
-        project_id = query[:project_id] || config.project_id
-
+      def get_directory_progress(directory_id = nil, query = {}, project_id = config.project_id)
         directory_id || raise_parameter_is_required_error(:directory_id)
         project_id   || raise_project_id_is_required_error
 
@@ -35,9 +31,7 @@ module Crowdin
         request.perform
       end
 
-      def get_file_progress(file_id = nil, query = {})
-        project_id = query[:project_id] || config.project_id
-
+      def get_file_progress(file_id = nil, query = {}, project_id = config.project_id)
         file_id    || raise_parameter_is_required_error(:file_id)
         project_id || raise_project_id_is_required_error
 
@@ -51,9 +45,7 @@ module Crowdin
         request.perform
       end
 
-      def get_language_progress(language_id = nil, query = {})
-        project_id = query[:project_id] || config.project_id
-
+      def get_language_progress(language_id = nil, query = {}, project_id = config.project_id)
         language_id || raise_parameter_is_required_error(:language_id)
         project_id  || raise_project_id_is_required_error
 
