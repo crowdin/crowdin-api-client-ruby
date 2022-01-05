6d6be37b5d61717890ea96ad86258eb363af0187
Korbut Kirill <kirill.korbut@roobykon.com>
2022-01-05 09:50:24 +0200

    Linter revisions

--- lib/crowdin-api/core/client_errors_raiser.rb
+++ lib/crowdin-api/core/client_errors_raiser.rb
@@ -4,7 +4,7 @@ module Crowdin
   module Errors
     module ClientErrorsRaiser
       def raise_logger_are_not_enabled_error
-        raise(LoggerAreNotEnabledError, 'Logger are not enabled in your Client configuration, enable it ' +
+        raise(LoggerAreNotEnabledError, 'Logger are not enabled in your Client configuration, enable it ' \
                                         'before setting your own logger')
       end
     end
