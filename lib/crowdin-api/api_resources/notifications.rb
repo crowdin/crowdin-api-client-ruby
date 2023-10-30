e04afe9dd7305c5e62f06f6f23897fb9fc147879
Mohan Thanigaivelan <mohanthanigaivelan@Mohans-MacBook-Pro.local>
2023-10-31 00:03:06 +0530

    Added support for send_notification_to_authenticated_user API

--- lib/crowdin-api/api_resources/notifications.rb
+++ lib/crowdin-api/api_resources/notifications.rb
@@ -1,7 +1,23 @@
 module Crowdin
   module ApiResources
     module Notifications
+
+      def send_notification_to_authenticated_user(query = {})
+
+        %i[message].each do |param|
+          query[param] || raise_parameter_is_required_error(param)
+        end
+
+        request = Web::Request.new(
+          connection,
+          :post,
+          "#{config.target_api_url}/notify",
+          { params: query }
+        )
+        Web::SendRequest.new(request).perform
+      end
       def send_notification_to_organization_member(query = {})
+        enterprise_mode? || raise_only_for_enterprise_mode_error
 
         %i[message].each do |param|
           query[param] || raise_parameter_is_required_error(param)
