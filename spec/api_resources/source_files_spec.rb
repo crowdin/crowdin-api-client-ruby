65db494cfaf5308fc153194e6509eb5e5ffeae72
Igor S. Froehner <48810009+IgorFroehner@users.noreply.github.com>
2024-10-22 03:13:06 -0300

    feat: Add the 'Download File Preview' API support (#98)

--- spec/api_resources/source_files_spec.rb
+++ spec/api_resources/source_files_spec.rb
@@ -160,6 +160,16 @@ describe Crowdin::ApiResources::SourceFiles do
       end
     end
 
+    describe '#download_file_preview' do
+      let(:file_id) { 1 }
+
+      it 'when request are valid', :default do
+        stub_request(:get, "https://api.crowdin.com/#{target_api_url}/projects/#{project_id}/files/#{file_id}/preview")
+        download_file_preview = @crowdin.download_file_preview(file_id, nil, project_id)
+        expect(download_file_preview).to eq(200)
+      end
+    end
+
     describe '#list_file_revisions' do
       let(:file_id) { 1 }
 
