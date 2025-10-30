b66e73c9dc16198e26f8c282d208deb5fed8bfd5
Oksana Rodionova <oxbesomhead@gmail.com>
2025-10-30 15:18:33 +0300

    docs: add method documentation for Dictionaries, Distributions and Glossaries APIs (#113)

--- lib/crowdin-api/api_resources/dictionaries.rb
+++ lib/crowdin-api/api_resources/dictionaries.rb
@@ -3,6 +3,9 @@
 module Crowdin
   module ApiResources
     module Dictionaries
+      # @param query [Hash] Request Body
+      # * {https://support.crowdin.com/developer/api/v2/#tag/Dictionaries/operation/api.projects.dictionaries.getMany  API Documentation}
+      # * {https://support.crowdin.com/developer/enterprise/api/v2/#tag/Dictionaries/operation/api.projects.dictionaries.getMany  Enterprise API Documentation}
       def list_dictionaries(query = {}, project_id = config.project_id)
         project_id || raise_project_id_is_required_error
 
@@ -15,6 +18,10 @@ module Crowdin
         Web::SendRequest.new(request).perform
       end
 
+      # @param language_id [String] Language Identifier
+      # @param query [Hash] Request Body
+      # * {https://support.crowdin.com/developer/api/v2/#tag/Dictionaries/operation/api.projects.dictionaries.patch  API Documentation}
+      # * {https://support.crowdin.com/developer/enterprise/api/v2/#tag/Dictionaries/operation/api.projects.dictionaries.patch  Enterprise API Documentation}
       def edit_dictionary(language_id = nil, query = {}, project_id = config.project_id)
         language_id || raise_parameter_is_required_error(:language_id)
         project_id  || raise_project_id_is_required_error
--- lib/crowdin-api/api_resources/distributions.rb
+++ lib/crowdin-api/api_resources/distributions.rb
@@ -3,6 +3,9 @@
 module Crowdin
   module ApiResources
     module Distributions
+      # @param query [Hash] Request Body
+      # * {https://support.crowdin.com/developer/api/v2/#tag/Distributions/operation/api.projects.distributions.getMany  API Documentation}
+      # * {https://support.crowdin.com/developer/enterprise/api/v2/#tag/Distributions/operation/api.projects.distributions.getMany  Enterprise API Documentation}
       def list_distributions(query = {}, project_id = config.project_id)
         project_id || raise_project_id_is_required_error
 
@@ -15,6 +18,9 @@ module Crowdin
         Web::SendRequest.new(request).perform
       end
 
+      # @param query [Hash] Request Body
+      # * {https://support.crowdin.com/developer/api/v2/#tag/Distributions/operation/api.projects.distributions.post  API Documentation}
+      # * {https://support.crowdin.com/developer/enterprise/api/v2/#tag/Distributions/operation/api.projects.distributions.post  Enterprise API Documentation}
       def add_distribution(query = {}, project_id = config.project_id)
         project_id || raise_project_id_is_required_error
 
@@ -27,6 +33,9 @@ module Crowdin
         Web::SendRequest.new(request).perform
       end
 
+      # @param hash [String] Hash
+      # * {https://support.crowdin.com/developer/api/v2/#tag/Distributions/operation/api.projects.distributions.get  API Documentation}
+      # * {https://support.crowdin.com/developer/enterprise/api/v2/#tag/Distributions/operation/api.projects.distributions.get  Enterprise API Documentation}
       def get_distribution(hash = nil, project_id = config.project_id)
         hash       || raise_parameter_is_required_error(:hash)
         project_id || raise_project_id_is_required_error
@@ -39,6 +48,9 @@ module Crowdin
         Web::SendRequest.new(request).perform
       end
 
+      # @param hash [String] Hash
+      # * {https://support.crowdin.com/developer/api/v2/#tag/Distributions/operation/api.projects.distributions.delete  API Documentation}
+      # * {https://support.crowdin.com/developer/enterprise/api/v2/#tag/Distributions/operation/api.projects.distributions.delete  Enterprise API Documentation}
       def delete_distribution(hash = nil, project_id = config.project_id)
         hash       || raise_parameter_is_required_error(:hash)
         project_id || raise_project_id_is_required_error
@@ -51,6 +63,10 @@ module Crowdin
         Web::SendRequest.new(request).perform
       end
 
+      # @param hash [String] Hash
+      # @param query [Hash] Request Body
+      # * {https://support.crowdin.com/developer/api/v2/#tag/Distributions/operation/api.projects.distributions.patch  API Documentation}
+      # * {https://support.crowdin.com/developer/enterprise/api/v2/#tag/Distributions/operation/api.projects.distributions.patch  Enterprise API Documentation}
       def edit_distribution(hash = nil, query = {}, project_id = config.project_id)
         hash       || raise_parameter_is_required_error(:hash)
         project_id || raise_project_id_is_required_error
@@ -64,6 +80,9 @@ module Crowdin
         Web::SendRequest.new(request).perform
       end
 
+      # @param hash [String] Hash
+      # * {https://support.crowdin.com/developer/api/v2/#tag/Distributions/operation/api.projects.distributions.release.get  API Documentation}
+      # * {https://support.crowdin.com/developer/enterprise/api/v2/#tag/Distributions/operation/api.projects.distributions.release.get  Enterprise API Documentation}
       def get_distribution_release(hash = nil, project_id = config.project_id)
         hash       || raise_parameter_is_required_error(:hash)
         project_id || raise_project_id_is_required_error
@@ -76,6 +95,9 @@ module Crowdin
         Web::SendRequest.new(request).perform
       end
 
+      # @param hash [String] Hash
+      # * {https://support.crowdin.com/developer/api/v2/#tag/Distributions/operation/api.projects.distributions.release.post  API Documentation}
+      # * {https://support.crowdin.com/developer/enterprise/api/v2/#tag/Distributions/operation/api.projects.distributions.release.post  Enterprise API Documentation}
       def release_distribution(hash = nil, project_id = config.project_id)
         hash       || raise_parameter_is_required_error(:hash)
         project_id || raise_project_id_is_required_error
