f1fe3f64dff1439762800a21b7b5424a25c2ab83
saranour700 <sara.nour813@gmail.com>
2026-03-05 15:20:13 +0200

    fix: add enterprise mode check to ai_translate_strings

--- lib/crowdin-api/api_resources/ai.rb
+++ lib/crowdin-api/api_resources/ai.rb
@@ -2,11 +2,14 @@
 
 module Crowdin
   module ApiResources
-    module AI
-      # https://support.crowdin.com/developer/api/v2/#tag/AI/operation/api.users.ai.translate.strings.post
+    module Ai
+      # AI Translate Strings
+      # @param user_id [Integer] User ID
+      # @param query [Hash] Request Body
+      # @see https://support.crowdin.com/developer/api/v2/#tag/AI/operation/api.users.ai.translate.strings.post
       def ai_translate_strings(user_id = nil, query = {})
-        user_id || raise_parameter_is_required_error(:user_id)
         enterprise_mode? || raise_only_for_enterprise_mode_error
+        user_id || raise_parameter_is_required_error(:user_id)
 
         request = Web::Request.new(
           connection,
