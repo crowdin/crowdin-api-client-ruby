bfb7fcb0ddc1deae0a283bb00104a063b72c6aae
saranour700 <sara.nour813@gmail.com>
2026-03-05 15:29:23 +0200

    fix: update ai_spec to use enterprise mode and improve tests

---
+++ spec/crowdin-api/api_resources/ai_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Crowdin::ApiResources::Ai do
+  describe '#ai_translate_strings' do
+    let(:user_id) { 1 }
+
+    it 'when request are valid', :enterprise do
+      stub_request(:post, "https://domain.api.crowdin.com/api/v2/users/#{user_id}/ai/translations/translate-strings")
+        .to_return(
+          status: 200,
+          body: {
+            data: {
+              translations: ['Hallo Welt']
+            }
+          }.to_json
+        )
+
+      result = @crowdin.ai_translate_strings(user_id, { strings: ['Hello'], targetLanguageId: 'de' })
+      expect(result['data']['translations']).to eq(['Hallo Welt'])
+    end
+
+    it 'should raise error when user_id is missing' do
+      expect { @crowdin.ai_translate_strings(nil, { strings: ['Hello'] }) }
+        .to raise_error(Crowdin::Errors::ParameterIsRequiredError)
+    end
+
+    it 'should raise error when not in enterprise mode' do
+      allow(@crowdin).to receive(:enterprise_mode?).and_return(false)
+      expect { @crowdin.ai_translate_strings(user_id, { strings: ['Hello'] }) }
+        .to raise_error(Crowdin::Errors::OnlyForEnterpriseModeError)
+    end
+  end
+end
