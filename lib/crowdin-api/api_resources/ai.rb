d0040befb7b8be2ef19617ee0f933cd5d20d138d
saranour700 <sara.nour813@gmail.com>
2026-03-07 11:23:23 +0200

    fix: support ai_translate_strings in both Crowdin and Enterprise modes

--- lib/crowdin-api/api_resources/ai.rb
+++ lib/crowdin-api/api_resources/ai.rb
@@ -6,15 +6,16 @@ module Crowdin
       # AI Translate Strings
       # @param user_id [Integer] User ID
       # @param query [Hash] Request Body
-      # @see https://support.crowdin.com/developer/api/v2/#tag/AI/operation/api.users.ai.translate.strings.post
+      # @see https://support.crowdin.com/developer/api/v2/#tag/AI/operation/api.users.ai.translate.strings.post 
       def ai_translate_strings(user_id = nil, query = {})
-        enterprise_mode? || raise_only_for_enterprise_mode_error
         user_id || raise_parameter_is_required_error(:user_id)
+
+        path = enterprise_mode? ? "/ai/translations/translate-strings" : "/users/#{user_id}/ai/translations/translate-strings"
 
         request = Web::Request.new(
           connection,
           :post,
-          "#{config.target_api_url}/users/#{user_id}/ai/translations/translate-strings",
+          "#{config.target_api_url}#{path}",
           { params: query }
         )
         Web::SendRequest.new(request).perform
--- spec/api_resources/ai_spec.rb
+++ spec/api_resources/ai_spec.rb
@@ -6,8 +6,10 @@ describe Crowdin::ApiResources::Ai do
   describe '#ai_translate_strings' do
     let(:user_id) { 1 }
 
-    it 'when request are valid', :enterprise do
-      stub_request(:post, "https://#{organization_domain}.api.crowdin.com/api/v2/users/#{user_id}/ai/translations/translate-strings")
+    it 'when request are valid in Crowdin mode' do
+      @crowdin = Crowdin::Client.new
+      
+      stub_request(:post, "https://api.crowdin.com/api/v2/users/#{user_id}/ai/translations/translate-strings")
         .to_return(
           status: 200,
           body: {
@@ -21,15 +23,26 @@ describe Crowdin::ApiResources::Ai do
       expect(result['data']['translations']).to eq(['Hallo Welt'])
     end
 
-    it 'should raise error when user_id is missing', :enterprise do
-      expect { @crowdin.ai_translate_strings(nil, { strings: ['Hello'] }) }
-        .to raise_error(ArgumentError)
+    it 'when request are valid in Enterprise mode' do
+      @crowdin = Crowdin::Client.new { |cfg| cfg.organization_domain = 'domain' }
+      
+      stub_request(:post, "https://domain.api.crowdin.com/api/v2/ai/translations/translate-strings")
+        .to_return(
+          status: 200,
+          body: {
+            data: {
+              translations: ['Hallo Welt']
+            }
+          }.to_json
+        )
+
+      result = @crowdin.ai_translate_strings(user_id, { strings: ['Hello'], targetLanguageId: 'de' })
+      expect(result['data']['translations']).to eq(['Hallo Welt'])
     end
 
-    it 'should raise error when not in enterprise mode' do
-      allow(@crowdin).to receive(:enterprise_mode?).and_return(false)
-      expect { @crowdin.ai_translate_strings(user_id, { strings: ['Hello'] }) }
-        .to raise_error(Crowdin::Errors::OnlyForEnterpriseModeError)
+    it 'should raise error when user_id is missing' do
+      expect { @crowdin.ai_translate_strings(nil, { strings: ['Hello'] }) }
+        .to raise_error(ArgumentError)
     end
   end
 end
